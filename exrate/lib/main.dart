5a5ae7e7f8fb18f649f1ec9a568825cf2a138a7f
MaccBass <besta.k0424@gmail.com>
2023-11-14 19:38:28 +0900

    modified:   exrate/build/7449af018a23ac727800ee791846b26e.cache.dill.track.dill 	modified:   exrate/build/c08fbf61d83fcdb66c6e55cde0dbfcb4/gen_dart_plugin_registrant.stamp 	modified:   exrate/lib/main.dart 	new file:   exrate/lib/widgets/currency_card.dart

--- exrate/lib/main.dart
+++ exrate/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'widgets/button.dart';
+import 'widgets/currency_card.dart';
 
 void main() {
   runApp(const App());
@@ -14,154 +15,120 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text('Hello, Insoo!',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 36,
-                            fontWeight: FontWeight.w800,
-                          )),
-                      Text('Welcome back.',
-                          style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 18)),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 80,
-              ),
-              Text(
-                'Total Balance',
-                style: TextStyle(
-                    fontSize: 22, color: Colors.white.withOpacity(0.8)),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                '\$5 697 151',
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 42,
-                    color: Colors.white),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Button(
-                      text: 'Transfer',
-                      bgColor: Colors.amber,
-                      textColor: Colors.black),
-                  Button(
-                      text: 'Request',
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white),
-                ],
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Wallets',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 36,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    'View All',
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(0.8),
-                      fontSize: 18,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Container(
-                clipBehavior: Clip.hardEdge,
-                decoration: const BoxDecoration(
-                  color: Color(0xFF1F2123),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Euro',
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text('Hello, Insoo!',
                             style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.w600),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            children: [
-                              const Text(
-                                '6 974',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 5,
-                              ),
-                              Text(
-                                'EUR',
-                                style: TextStyle(
-                                    color: Colors.white.withOpacity(0.8),
-                                    fontSize: 20),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      Transform.scale(
-                        scale: 2.2,
-                        child: Transform.translate(
-                          offset: const Offset(-5, 10),
-                          child: const Icon(
-                            Icons.euro_rounded,
-                            color: Colors.white,
-                            size: 72,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                              color: Colors.white,
+                              fontSize: 36,
+                              fontWeight: FontWeight.w800,
+                            )),
+                        Text('Welcome back.',
+                            style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 18)),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 60,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                      fontSize: 22, color: Colors.white.withOpacity(0.8)),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  '\$5 697 151',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 42,
+                      color: Colors.white),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Button(
+                        text: 'Transfer',
+                        bgColor: Colors.amber,
+                        textColor: Colors.black),
+                    Button(
+                        text: 'Request',
+                        bgColor: Color(0xFF1F2123),
+                        textColor: Colors.white),
+                  ],
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Wallets',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'View All',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const CurrencyCard(
+                  name: 'Euro',
+                  code: 'EUR',
+                  amount: '6 974',
+                  icon: Icons.euro_rounded,
+                  isInverted: false,
+                  order: 1,
+                ),
+                const CurrencyCard(
+                  name: 'Dollar',
+                  code: 'USD',
+                  amount: '58 995',
+                  icon: Icons.attach_money_rounded,
+                  isInverted: true,
+                  order: 2,
+                ),
+                const CurrencyCard(
+                  name: 'Yen',
+                  code: 'JPY',
+                  amount: '7 995 618',
+                  icon: Icons.currency_yen_rounded,
+                  isInverted: false,
+                  order: 3,
+                ),
+              ],
+            ),
           ),
         ),
       ),
